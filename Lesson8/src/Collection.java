5083334c0098d57a436e18d8610ba21ae7d5d13c
VadPol95 <vpolyvyanoy@gmail.com>
2022-06-13 23:02:13 +0300

    Re-work

--- Lesson8/src/Collection.java
+++ Lesson8/src/Collection.java
@@ -3,7 +3,7 @@ public interface Collection {
     boolean add(String o);
     boolean add(int index, String o);
     boolean delete(String o);
-    Object get(int index);
+    String get(int index);
     boolean contain(String o);
     boolean equals(Collection str);
     boolean clear();
